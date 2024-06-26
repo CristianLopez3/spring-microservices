355d2ab479d3a690b7fd94af30f6023f3b863335
CristianLopez3 <cristian.c.lopez.m@gmail.com>
2024-06-26 18:05:19 -0500

    delete sections

--- section2/accounts/src/main/java/com/easybytes/accounts/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.easybytes.accounts.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
-        super(String.format("%s not found with given input data %s : '%s'", resourceName, fieldName, fieldValue));
-    }
-
-}
