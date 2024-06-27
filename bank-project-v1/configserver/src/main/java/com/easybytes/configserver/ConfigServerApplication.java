3cf69f47bf45143cb13698bee893ec35893e1577
CristianLopez3 <cristian.c.lopez.m@gmail.com>
2024-06-26 21:26:45 -0500

    add properties to config server, in order to centralize over all configurations

--- bank-project-v1/configserver/src/main/java/com/easybytes/configserver/ConfigServerApplication.java
+++ bank-project-v1/configserver/src/main/java/com/easybytes/configserver/ConfigServerApplication.java
@@ -2,12 +2,14 @@ package com.easybytes.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class ConfigserverApplication {
+@EnableConfigServer
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigserverApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
